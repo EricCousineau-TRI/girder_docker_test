2fcc0cd4d62b31816ef1952cc3878523b02679de
Eric Cousineau <eric.cousineau@tri.global>
2017-11-08 11:22:03 -0500

    Add instructions

--- setup/docker/build.sh
+++ setup/docker/build.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 set -e -u
 
+cd $(dirname $0)
 docker build -t girder-dev -f ./Dockerfile.girder-dev .
 docker build -t girder_mongodb -f ./Dockerfile .
+
+cat <<EOF
+
+For running:
+
+    cd test_with_girder
+    docker rm -f girder_mongodb_c || :
+    docker run --name girder_mongodb_c --entrypoint bash --detach -t -p 8080:8080 -v ~+:/mnt girder_mongodb
+    docker exec -it girder_mongodb_c bash
+
+EOF
